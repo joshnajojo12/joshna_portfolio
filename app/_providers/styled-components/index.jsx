d30c142445d04ec4b35a18989c1016d1552aaf24
joshna jojo <joshnajojo200621@gmail.com>
2026-02-04 01:02:15 +0530

    Fix styled-components hydration

--- app/_providers/styled-components/index.jsx
+++ app/_providers/styled-components/index.jsx
@@ -23,7 +23,9 @@ export function StyledComponentsRegistry({ children }) {
     return <>{styles}</>;
   });
 
-  if (typeof window !== 'undefined') return <>{children}</>;
+  if (typeof window !== 'undefined') {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  }
 
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
